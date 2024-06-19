bcf1946f4751d281e97eab2fa4ab0bb18d59c89d
RenaldasZ <spikeris2@gmail.com>
2024-06-19 18:38:19 +0300

    docstrings are added for Main game class

--- client/src/game_logic/game.py
+++ client/src/game_logic/game.py
@@ -18,6 +18,8 @@ from src.utils.barrier import collides_with_barrier
 from src.utils.sprite_sheet import SpriteSheet
 
 class Game:
+    """Main game class responsible for initializing and managing the game state, including player, NPCs, enemies, and game events."""
+    
     TILE_WELL = TILE_WELL
     TILE_TREE = TILE_TREE
 
@@ -28,7 +30,13 @@ class Game:
     }
 
     def __init__(self, screen_size, screen):
-        # Initialize game components
+        """
+        Initialize the Game instance.
+
+        Args:
+            screen_size (tuple): The size of the game screen (width, height).
+            screen (pygame.Surface): The surface to render the game on.
+        """
         self.screen_size = screen_size
         self.screen = screen
         self.running = True
@@ -61,6 +69,7 @@ class Game:
         self.spawn_manager.spawn_enemies()
 
     def handle_events(self):
+        """Handle game events such as player inputs, NPC interactions, and transitions."""
         self.input_handler.handle_events()
         self.check_transition_area_collision()
         self.handle_npc_interaction(NPC.TILE_NPC_1)
@@ -69,10 +78,17 @@ class Game:
         self.interaction_manager.check_interaction()
 
     def check_transition_area_collision(self):
+        """Check if the player collides with the transition area to trigger a map transition."""
         if not self.transitioning and self.player.rect.colliderect(self.transition_area):
             self.transition_manager.transition_to_second_map()
 
     def handle_npc_interaction(self, tile_id):
+        """
+        Handle interactions with NPCs.
+
+        Args:
+            tile_id (int): The tile ID of the NPC.
+        """
         if self.map_tiles[int(self.player._y) // self.CHUNK_SIZE][int(self.player._x) // self.CHUNK_SIZE] == tile_id:
             if tile_id == NPC.TILE_NPC_1:
                 self.npc.handle_interaction(self.player, tile_id)
@@ -80,7 +96,7 @@ class Game:
                 self.npc.handle_interaction(self.player, tile_id)
 
     def handle_player_attack(self):
-        # Check for collisions between player and enemies
+        """Handle player's attacks on enemies and update enemy states accordingly."""
         for enemy in self.enemies:
             if enemy.alive:
                 distance_to_enemy = math.sqrt((self.player._x - enemy.x) ** 2 + (self.player._y - enemy.y) ** 2)
@@ -92,35 +108,70 @@ class Game:
                         self.respawn_enemy(enemy)
 
     def respawn_enemy(self, enemy):
-        # Find the index of the dead enemy
+        """
+        Respawn a dead enemy at its initial position.
+
+        Args:
+            enemy (Enemy): The enemy to respawn.
+        """
         index = self.enemies.index(enemy)
-        # Respawn the enemy at its initial position
         self.enemies[index] = Enemy(enemy.initial_x, enemy.initial_y)
 
     def handle_mouse_click(self, mouse_pos):
+        """
+        Handle mouse click events and set the target position for the player.
+
+        Args:
+            mouse_pos (tuple): The position of the mouse click (x, y).
+        """
         self.target_pos = self.screen_to_map(mouse_pos)
 
     def screen_to_map(self, screen_pos):
+        """
+        Convert screen coordinates to map coordinates.
+
+        Args:
+            screen_pos (tuple): The position on the screen (x, y).
+
+        Returns:
+            tuple: Corresponding map coordinates (x, y).
+        """
         return (
             screen_pos[0] + self.player._x - self.screen_size[0] // 2,
             screen_pos[1] + self.player._y - self.screen_size[1] // 2
         )
 
     def update(self):
+        """Update the game state, including player and enemy updates."""
         self.player_manager.update()
-        # Update all enemies
         for enemy in self.enemies:
             if enemy.alive:
                 enemy.update(self.player)
                 enemy.attack_player(self.player)
 
     def render(self):
+        """Render the game state on the screen."""
         self.renderer.render()
         self.player_renderer.render_player_coords()
         self.player_renderer.render_player_health()
 
     def get_player_chunk(self):
+        """
+        Get the current chunk coordinates of the player.
+
+        Returns:
+            tuple: The chunk coordinates (chunk_x, chunk_y).
+        """
         return self.player_manager.get_player_chunk()
 
     def collides_with_barrier(self, pos):
+        """
+        Check if a position collides with any barriers on the map.
+
+        Args:
+            pos (tuple): The position to check (x, y).
+
+        Returns:
+            bool: True if the position collides with a barrier, False otherwise.
+        """
         return collides_with_barrier(pos, self.map_tiles, self.CHUNK_SIZE)
