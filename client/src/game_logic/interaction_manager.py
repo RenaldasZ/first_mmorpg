ff68b8697f299f6dfd972d0e7f91d762966f399e
RenaldasZ <spikeris2@gmail.com>
2024-06-19 19:44:21 +0300

    added dockstrings to interaction manager

--- client/src/game_logic/interaction_manager.py
+++ client/src/game_logic/interaction_manager.py
@@ -1,19 +1,38 @@
 # src/game_logic/interaction_manager.py
 class InteractionManager:
+    """
+    Manages player interactions with objects in the game world.
+    """
+
     def __init__(self, game):
+        """
+        Initializes the InteractionManager.
+
+        Args:
+            game (Game): The game instance.
+        """
         self.game = game
 
     def handle_well_interaction(self):
+        """
+        Handles interaction with a well, healing the player and possibly converting an empty vial to a vial of water.
+        """
         self.game.player.heal(0.1)
         if "Empty vial" in self.game.player.inventory.items and self.game.quest_handler.empty_vial_returned:
             self.game.player.remove_item_from_inventory("Empty vial")
             self.game.player.add_to_inventory("Vial of Water")
 
     def handle_tree_interaction(self):
+        """
+        Handles interaction with a tree, adding a stick to the player's inventory if not already present.
+        """
         if "Stick" not in self.game.player.inventory.items:
             self.game.player.add_to_inventory("Stick")
 
     def check_interaction(self):
+        """
+        Checks for player interactions with nearby objects such as wells or trees.
+        """
         # Get the player's position in terms of chunks
         player_x_chunk = int((self.game.player._x + self.game.player._size / 2) // self.game.CHUNK_SIZE)
         player_y_chunk = int((self.game.player._y + self.game.player._size / 2) // self.game.CHUNK_SIZE)
